3d5d76215a189967c768059df4db26fc4a2639e8
Héctor Enríquez <hectoren@gmail.com>
2026-01-14 16:47:52 -0500

    Player sprites sliced. First animation idle. Collision with gravity force and horizontal movement.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float inputH;
+    [SerializeField] private float movementVelocity;
+    [SerializeField] private float jumpForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputH = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(inputH * movementVelocity, rb.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
